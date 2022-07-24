9dcf584debff424c5e52b43a2f52a1f011224f6d
remi <hellokiziche@gmail.com>
2022-07-24 20:45:40 +0200

    plugin_images

---
+++ app/src/examples/Images.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+import styled from "styled-components"
+
+const Images = () => {
+  return (
+    <Wrapper>
+      <h2>gatsby image</h2>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.section``
+
+export default Images
--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import Images from '../examples/Images'
 
 export default function Home() {
   return (
     <Layout>
       <h1>Home Page</h1>
+      <Images />
     </Layout>
   )
 }
