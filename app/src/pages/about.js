d72882ef1c7e4ce6c86ee7101283240119dffe65
remi <hellokiziche@gmail.com>
2022-07-24 15:55:07 +0200

    Layout_componenet

--- app/src/pages/about.js
+++ app/src/pages/about.js
@@ -1,11 +1,14 @@
 import React from "react"
+import Layout from "../components/Layout"
 
 const about = () => {
-    return (
-        <div>
-            hello from about page
-        </div>
-    )
+  return (
+    <Layout>
+      <div>
+        <h2>hello from about page</h2>
+      </div>
+    </Layout>
+  )
 }
 
 export default about
--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,5 +1,17 @@
 import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/Layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <h1> Home page </h1>
+      <div>
+        <Link to="/about">about</Link>
+      </div>
+      <div>
+        <Link to="/company/history">history</Link>
+      </div>
+    </Layout>
+  )
 }
